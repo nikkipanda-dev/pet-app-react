c1dfecbc7e81fcd08eb987fd10b3e2bcc6b53eca
Nikki <62213020+nikkipanda-dev@users.noreply.github.com>
2022-02-07 04:12:46 +0800

    Updates user display photo

--- src/components/pages/settings/profile/index.js
+++ src/components/pages/settings/profile/index.js
@@ -13,13 +13,10 @@ import Button from "../../../core/Button";
 import Image from "../../../core/Image";
 
 export const ProfileSettings = () => {
-
     const username = JSON.parse(Cookies.get('x_auth_user'))['username'];
 
     const [displayPhotoPath, setDisplayPhotoPath] = useState(null);
     const displayPhotoRef = useRef();
-
-    console.log('path: ', JSON.parse(Cookies.get('x_auth_user_display_photo')));
 
     const triggerFileSelect = evt => {
         evt.current.click();
@@ -39,6 +36,8 @@ export const ProfileSettings = () => {
             if (addPhotoRes.isSuccess) {
                 Cookies.set('x_auth_user_display_photo', JSON.stringify(addPhotoRes.data['image_path']), { sameSite: 'strict', secure: true });
 
+                setDisplayPhotoPath(addPhotoRes.data['image_path']);
+
                 // clear input field
                 displayPhotoRef.current.value = '';
             } else {
@@ -48,6 +47,34 @@ export const ProfileSettings = () => {
 
         .catch (err => {
             console.log('err photo res', err.response.data.errors)
+        })
+    }
+
+    const changeDisplayPhoto = evt => {
+        evt.preventDefault();
+
+        const updatePhotoForm = new FormData(evt.target)
+        updatePhotoForm.append('id', JSON.parse(Cookies.get('x_auth_user'))['id']);
+
+        axiosDef.post('http://localhost:8000/api/user/' + username + '/settings/display-photo/update', updatePhotoForm)
+
+        .then (res => {
+            const changePhotoRes = res.data;
+
+            if (changePhotoRes) {
+                Cookies.set('x_auth_user_display_photo', JSON.stringify(changePhotoRes.data['image_path']), { sameSite: 'strict', secure: true });
+
+                setDisplayPhotoPath(changePhotoRes.data['image_path']);
+    
+                // clear input field
+                displayPhotoRef.current.value = '';
+            } else {
+                console.log('err change:" ', changePhotoRes.data);
+            }
+        })
+
+        .catch (err => {
+            console.log('err upd ', err)
         })
     }
 
@@ -69,6 +96,8 @@ export const ProfileSettings = () => {
                     imgStyle={{ objectFit: 'cover', width: '100%', height: '100%', maxWidth: '400px', maxHeight: '400px', }}/>
                 </Column>
                 <Column>
+                {
+                    !(displayPhotoPath) ? 
                     <Form
                     action='#'
                     method='POST'
@@ -86,7 +115,28 @@ export const ProfileSettings = () => {
                         accept='image/*'
                         hidden={ true }/>
                         <Button type='submit' text='Save'/>
+                    </Form> :
+                    <>
+                        <Form
+                        action='#'
+                        method='POST'
+                        encType='multipart'
+                        onSubmit={ changeDisplayPhoto }>
+                        <Label
+                        text='Select' 
+                        labelOnclick={ triggerFileSelect } 
+                        refTarget={ displayPhotoRef }/>
+                        <Input 
+                        fieldType='file'
+                        type='file' 
+                        refTarget={ displayPhotoRef } 
+                        name='display_photo' 
+                        accept='image/*'
+                        hidden={ true }/>
+                        <Button type='submit' text='Change display photo'/>
                     </Form>
+                    </>
+                }
                </Column>
             </Row>
         </Container>
